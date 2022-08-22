e1a62ac47ff7030996f2bb49ca360d87f3a2f6ef
lilys tacos <lilyannek@gmail.com>
2022-08-22 15:57:56 -0700

    added new WebpackPwaManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,22 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "A text editor with offiline capabilities",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
     module: {
       rules: [],
